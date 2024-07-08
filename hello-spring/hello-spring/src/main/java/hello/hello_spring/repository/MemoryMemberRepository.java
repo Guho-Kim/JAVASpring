1bb190d707663f7085609e296dde262c0f8492c8
Guho-Kim <weeeeek2@naver.com>
2024-07-08 18:26:41 +0900

    feature: JpaMemberRepository

--- hello-spring/hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ hello-spring/hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -1,14 +1,12 @@
 package hello.hello_spring.repository;
-
 import hello.hello_spring.domain.Member;
-
+import org.springframework.stereotype.Repository;
 import java.util.*;
 
+//@Repository
 public class MemoryMemberRepository implements MemberRepository{
-
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
-
 
     @Override
     public Member save(Member member) {
@@ -16,8 +14,6 @@ public class MemoryMemberRepository implements MemberRepository{
         store.put(member.getId(), member);
         return member;
     }
-
-
 
     @Override
     public Optional<Member> findById(Long id) {
